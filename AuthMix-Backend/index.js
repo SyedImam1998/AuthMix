8841f19ed0a731fe63dd2c0b69a176ec5b6bdbd1
syed imam <syedimam1998@gmail.com>
2024-02-26 23:41:25 +0530

    2 login ui created

--- AuthMix-Backend/index.js
+++ AuthMix-Backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors({origin:"http://localhost:5173",credentials: true,}));
+app.use(cors({origin:"http://localhost:5173",credentials: true,})); // for cookies creds need to be added
 
 app.use('/user',LoginRoute)
 
--- AuthMix-Frontend/src/App.jsx
+++ AuthMix-Frontend/src/App.jsx
@@ -1,50 +1,89 @@
 import React from "react";
-import axios from 'axios';
+import axios from "axios";
 import "./App.css";
 // import axios from '../axios-config';
 
 function App() {
-  const [inputs,setInputs]=React.useState({});
+  const [inputs, setInputs] = React.useState({});
 
   const captureInputs = (name, value) => {
-   setInputs({
-     ...inputs,
-    [name]:value,
-  })
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
   };
 
-  const sendLoginCreds=async(e)=>{
-    e.preventDefault()
+  const sendLoginCreds = async (e) => {
+    e.preventDefault();
     console.log(inputs);
-    await axios.post('http://localhost:4000/user/login',inputs,{withCredentials:true}).then((data)=>{
-      console.log(data)
-    })
+    await axios
+      .post("http://localhost:4000/user/login", inputs, {
+        withCredentials: true,
+      })
+      .then((data) => {
+        console.log(data);
+      });
+  };
 
-  }
-
-  const getData=async()=>{
-    await axios.get('http://localhost:4000/data/allData',{withCredentials:true}).then((res)=>{
-      console.log(res);
-    })
-  }
+  const getData = async () => {
+    await axios
+      .get("http://localhost:4000/data/allData", { withCredentials: true })
+      .then((res) => {
+        console.log(res);
+      });
+  };
   return (
-    <>
-      <form
-       onSubmit={sendLoginCreds}
-      >
-        <input type="text" name="email" onChange={(e)=>captureInputs(e.target.name,e.target.value)}></input>
-        <input type="text" name="password" onChange={(e)=>captureInputs(e.target.name,e.target.value)}></input>
-        <br/>
-        <button type="submit">Send</button>
-      </form>
+    <div className="mainApp">
+      <div className="session-based">
+        <h4> Session based Login !!!</h4>
+        <form onSubmit={sendLoginCreds}>
+          <input
+            type="text"
+            placeholder="email"
+            name="email"
+            onChange={(e) => captureInputs(e.target.name, e.target.value)}
+          ></input>
+          <br/>
+          <br/>
+          
+          <input
+            type="text"
+            name="password"
+            placeholder="password"
+            onChange={(e) => captureInputs(e.target.name, e.target.value)}
+          ></input>
+          <br />
+          <br />
+          <button type="submit">Send</button>
+        </form>
+      </div>
 
-      <br/>
+      <br />
+      <div className="jwt-based">
+        <h4> JWT based Login !!!</h4>
+        <form onSubmit={sendLoginCreds}>
+          <input
+            type="text"
+            placeholder="email"
+            name="email"
+            onChange={(e) => captureInputs(e.target.name, e.target.value)}
+          ></input>
+          <br/>
+          <br/>
+          
+          <input
+            type="text"
+            name="password"
+            placeholder="password"
+            onChange={(e) => captureInputs(e.target.name, e.target.value)}
+          ></input>
+          <br />
+          <br />
+          <button type="submit">Send</button>
+        </form>
+      </div>
       <button onClick={getData}>Another Api Call</button>
-      
-
-
-
-    </>
+    </div>
   );
 }
 
